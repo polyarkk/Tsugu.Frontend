0d25366547faa32d96ac01f32580366d103860a6
brz <1781176460@qq.com>
2024-10-08 22:51:30 +0800

    impl | SongMeta

---
+++ Lagrange.Tsugu/Api/Endpoint/SongMeta.cs
@@ -0,0 +1,31 @@
+﻿using Lagrange.Tsugu.Api.Enum;
+using Lagrange.Tsugu.Api.Rest;
+using Lagrange.Tsugu.Command;
+
+namespace Lagrange.Tsugu.Api.Endpoint;
+
+[ApiCommand(
+    Alias = "查分数表",
+    Description = "查询歌曲分数排行表",
+    UsageHint = "[cn|jp|tw|kr|en]"
+)]
+public class SongMeta : BaseCommand {
+    public async override Task Invoke(Context ctx, ParsedCommand args) {
+        var p = new Dictionary<string, object?> {
+            ["displayedServerList"] = new[] { 3, 0 },
+            ["mainServer"] = args.GetEnum<BandoriServer>(0) ?? BandoriServer.Cn
+        };
+
+        using SugaredHttpClient rest = ctx.Rest;
+
+        RestResponse response = (await rest.TsuguPost("/songMeta", p))[0];
+
+        if (response.IsImageBase64()) {
+            await ctx.SendImage(response.String!);
+        } else {
+            await ctx.SendPlainText(
+                (string.IsNullOrWhiteSpace(response.String) ? "错误：请求失败" : response.String) + "！"
+            );
+        }
+    }
+}
